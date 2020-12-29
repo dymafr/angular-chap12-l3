bd69f23232e88ceb009c3810ebe91301a55a1df3
dymafr <erwan1309@hotmail.com>
2020-12-29 17:57:54 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  public form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      nom: new FormControl(""),
+      email: new FormControl(""),
+      password: new FormControl("")
+    });
+  }
+
+  submit() {
+    console.log(this.form);
+  }
+}
